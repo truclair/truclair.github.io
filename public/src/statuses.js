671a530723b431dd74dece4fbe559947821e2998
sfoonpork <sfoonpork@gmail.com>
2026-08-04 11:00:22 -0700

    Updated status labels

--- public/src/statuses.js
+++ public/src/statuses.js
@@ -1,7 +1,7 @@
 export const COMMISSION_STATUS_LABELS = {
     Pending: "Pending",
-    Accepted: "Accepted - Awaiting Payment",
-    Paid: "Payment Received - Awaiting Progress",
+    Accepted: "Invoice Sent - Awaiting Payment",
+    Paid: "Payment Received - Starting Soon!",
     Sketch: "Sketch - 0% to 25% Complete",
     Lineart: "Lineart - 25% to 50% Complete",
     Coloring: "Coloring - 50% to 75% Complete",
